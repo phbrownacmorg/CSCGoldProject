eab0013d27996ec530e82bc21cea98a1bcf2ee0f
WilliamELopez <williamelopez2021@gmail.com>
2025-04-08 14:24:45 -0400

    Update readExcel.py
    works, also gets the name from the excel file, still need to get the name from the instructors file

--- csc492proj/readExcel.py
+++ csc492proj/readExcel.py
@@ -1,9 +1,3 @@
-#import pandas as pd 
-
-#dataframe1 = pd.read_excel('EDU591.Y2_Students_anonymized.xlsx')
-
-#print(dataframe1)
-
 import pandas as pd
 import os
 import glob
@@ -17,13 +11,22 @@ else:
 
     for excel_file in excel_files:
         print(f"Processing: {excel_file}")
-   
+        
+        base_name = os.path.splitext(excel_file)[0]
+        parts = base_name.split('_')
+        
+        if parts and ',' in parts[-1]:
+            prof_last_name = parts[-1].split(',')[0]
+        else:
+            prof_last_name = 'Unknown'
+
+        print(f"Professor Last Name Detected: {prof_last_name}")
+
         df = pd.read_excel(excel_file)
-        
-       
-        csv_file = os.path.splitext(excel_file)[0] + '.csv'
-      
+
+        csv_file = base_name + '.csv'
+
         df.to_csv(csv_file, index=False)
-        
+
         print(f"Successfully converted to: {csv_file}")
         print(f"Preview of the data:\n{df.head()}\n")
